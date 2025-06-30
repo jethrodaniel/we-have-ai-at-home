351f4dd01d0d2c1edc4cfc5cec3d7d756ebbf9e0
Mark Delk <jethrodaniel@gmail.com>
2025-06-30 15:39:00 -0500

    ruby-llm model setup

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,2 +1,5 @@
 class Chat < ApplicationRecord
+  acts_as_chat
+
+  validates :model_id, presence: true
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
+  acts_as_message
+
   belongs_to :chat
   belongs_to :tool_call
+
+  validates :role, presence: true
 end
