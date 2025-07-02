95d269a7920ab215ec4947bd6883eafb18f6cc64
Mark Delk <jethrodaniel@gmail.com>
2025-07-02 11:10:36 -0500

    more flash cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def alert_class type
+    case type
+    in "notice" then "alert-success"
+    in "alert" then "alert-danger"
+    end
+  end
+
+  def alert_image type
+    case type
+    in "notice" then "check-circle.svg"
+    in "alert" then "exclamation-circle.svg"
+    end
+  end
 end
